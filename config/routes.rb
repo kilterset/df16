ae25578b41540adc29b96cbeaa7cd58c0d257485
Max Brosnahan <maximilianbrosnahan@gmail.com>
2016-09-06 11:38:05 +0200

    Add loaderio route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   root to: 'root#index'
 
+  loaderio_token = ENV['LOADERIO_TOKEN']
+
+  if loaderio_token.present?
+    get "/#{loaderio_token}", to: proc { [200, {}, [loaderio_token]] }
+  end
+
   namespace :api do
     resources :contacts, only: [:index, :show] do
       member do
